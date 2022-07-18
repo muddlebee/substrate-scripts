2bead9426ff183f76bb2e18f752f54c028070e6e
Anwesh <anweshknayak@gmail.com>
2022-07-18 19:49:12 +0530

    Update install-rust.sh

--- install-rust.sh
+++ install-rust.sh
@@ -1,3 +1,4 @@
+sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup default stable
